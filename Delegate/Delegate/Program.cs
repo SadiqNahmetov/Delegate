9044f18d1b11179eeafcb1627d5c816f4b01de4c
SadiqNahmetov <sadigfn@code.edu.az>
2022-07-13 06:49:22 +0400

    Delegate Homework

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegate
+{
+    //public delegate bool CheckNums(int num);
+
+    public delegate void ChangeString(string str);
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+           
+            int[] arr = { 1, 2, 3, 5, 6, 8, 10 };
+            //Console.WriteLine(Sum(CheckOdd, arr));
+            //Console.WriteLine(Sum(CehckEven, arr));
+            //Console.WriteLine(Sum(ChecThanFive,arr));
+
+            //List<int> numbers = new List<int>() { 2, 3, 5, 6, 8, 9, 11 };
+
+            //var result = numbers.FindAll(m => m >6);
+
+            //var result2 = numbers.FindAll(ChecThanFive);
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            Test(StringToUpper, "Sadiq");
+        }
+
+        //public static void Car() => Console.WriteLine("Cars");
+
+        public static void StringToUpper(string word)
+        {
+            Console.WriteLine(word.ToUpper());
+        }
+
+        public static void StringToLower(string word)
+        {
+            Console.WriteLine(word.ToLower());
+        }
+
+        public static void Test(ChangeString func, string word)
+        {
+            func(word);
+        }
+        //public static bool CheckOdd(int number)
+        //{
+        //    return number % 2 != 0;
+        //}
+        //public static bool CehckEven(int number)
+        //{
+        //    return number % 2 == 0;
+        //}
+
+
+        //public static bool ChecThanFive(int number)
+        //{
+        //    return number > 8;
+        //}
+
+        //public static int Sum(CheckNums func, int[] nums)
+        //{
+        //    var sum = 0;
+        //    foreach (var item in nums)
+        //    {
+        //        if (func(item))
+        //        {
+        //            sum += item;
+        //        }
+        //    }
+        //    return sum;
+        //}
+        //public static int SumEven(CheckNums func, int[] nums)
+        //{
+        //    var sum = 0;
+        //    foreach (var item in nums)
+        //    {
+        //        if (func(item))
+        //        {
+        //            sum += item;
+        //        }
+        //    }
+        //    return sum;
+        //}
+
+        //public static int SumThanFive(int[] nums)
+        //{
+        //    var sum = 0;
+        //    foreach (var item in nums)
+        //    {
+        //        if (item > 5)
+        //        {
+        //            sum += item;
+        //        }
+        //    }
+        //    return sum;
+        //}
+    }
+}
---
+++ Homework_Assignment/Delegate/Delegate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegate
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<string> strlist = new List<string>(); //{ "Sadiq", "Esqin", "Cavid", "Orxan", "Ramil"};
+            strlist.Add("Sadiq");
+            strlist.Add("Esqin");
+            strlist.Add("Cavid");
+            strlist.Add("Orxan");
+            strlist.Add("Ramil");
+            
+            ChekWord(CheckName, strlist);
+        }
+
+
+        public static bool CheckName(string name)
+        {
+            return name != "Cavid";
+        }
+
+
+        public static string ChekWord(Predicate<string> predicate, List<string> names)
+        {
+            var name = "Cavid";
+            foreach (var item in names)
+            {
+                if (predicate(item))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            return name;
+            
+        }
+    }
+
+       
+ 
+}
